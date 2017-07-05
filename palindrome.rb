1800f138cdc6469181e7ba61e2d9b48906937b83
ShubhamNandanwar <shubhamnandanwar9776@gmail.com>
2017-07-05 15:27:37 +0530

    Palindrome Programe Updated

--- palindrome.rb
+++ palindrome.rb
@@ -1,21 +1,20 @@
 class Palindrome
-	def is_palindrome(input_string)
-		array_length = input_string.length - 1
-		reverse_string = ""
-		input_string.each_char.with_index do |array_char,index|
-			 reverse_string += input_string[array_length - index]
-		end
-		return true if input_string == reverse_string
-		return false
-	end	
+  def is_palindrome(input)
+    array_length = input.length - 1
+
+    input[0, input.length / 2].each_char.with_index do |array_char,index|
+      return false if input[index] != input[array_length - index]
+    end
+    true
+  end 
 end
 
 puts("Enter an String\n")
-input_string = gets.chomp
+input = gets.chomp
 
 palindrome = Palindrome.new
-if palindrome.is_palindrome(input_string)
- puts "#{input_string} is a palindrome"
+if palindrome.is_palindrome(input)
+ puts "#{input} is a palindrome"
 else
- puts "#{input_string} is not a palindrome"
+ puts "#{input} is not a palindrome"
 end
