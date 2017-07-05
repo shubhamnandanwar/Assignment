b9e8e620e7dde27ea2a1bbad1b3587b78098dcb9
ShubhamNandanwar <shubhamnandanwar9776@gmail.com>
2017-07-05 17:11:21 +0530

    Replaced instance method by class method

--- palindrome.rb
+++ palindrome.rb
@@ -1,5 +1,5 @@
 class Palindrome
-  def is_palindrome(input)
+  def self.is_palindrome(input)
     array_length = input.length - 1
 
     input[0, input.length / 2].each_char.with_index do |array_char,index|
@@ -12,8 +12,7 @@ end
 puts("Enter an String\n")
 input = gets.chomp
 
-palindrome = Palindrome.new
-if palindrome.is_palindrome(input)
+if Palindrome.is_palindrome(input)
  puts "#{input} is a palindrome"
 else
  puts "#{input} is not a palindrome"
