a62e0810cc96935aeb3266fbc8357938955e7ee9
ShubhamNandanwar <shubhamnandanwar9776@gmail.com>
2017-07-06 14:59:34 +0530

    Replaced for loop by each

--- reverse_array.rb
+++ reverse_array.rb
@@ -1,5 +1,4 @@
 class Array
-
   def reverse_n
     self[0, self.length / 2].each_with_index do |input_element,index|
       self[index], self[self.length - 1 - index] = self[self.length - 1 - index], self[index]
@@ -10,13 +9,12 @@ end
 puts "Enter the size of the array\n"
 n = gets.chomp.to_i
 puts "\nEnter the Array Elements"
-input_array=Array.new(n)
+input = Array.new(n)
 
-for i in (0...n)
-  input_array[i]=gets.chomp.to_i
+(0...n).each do |index|
+  input[index]=gets.chomp.to_i
 end
 
 puts "\n\nReverse of Array"
-input_array.reverse_n
-puts input_array
-
+input.reverse_n
+puts input
