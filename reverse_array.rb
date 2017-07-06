af2ab96b5db6cc61f2262968c3e34b9f769d2037
ShubhamNandanwar <shubhamnandanwar9776@gmail.com>
2017-07-06 16:47:41 +0530

    Replace each loop by time

--- reverse_array.rb
+++ reverse_array.rb
@@ -1,7 +1,8 @@
 class Array
   def reverse_n
-    self[0, self.length / 2].each_with_index do |input_element,index|
-      self[index], self[self.length - 1 - index] = self[self.length - 1 - index], self[index]
+  	length = self.length
+    self[0, length / 2].each_with_index do |input_element,index|
+      self[index], self[length - 1 - index] = self[length - 1 - index], self[index]
     end
   end
 end
@@ -11,7 +12,7 @@ n = gets.chomp.to_i
 puts "\nEnter the Array Elements"
 input = Array.new(n)
 
-(0...n).each do |index|
+n.times do |index|
   input[index]=gets.chomp.to_i
 end
 
