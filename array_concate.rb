e47d44b034135e1c3327092c159144db733e9879
ShubhamNandanwar <shubhamnandanwar9776@gmail.com>
2017-07-06 17:07:23 +0530

    each loop is replaced by times

--- array_concate.rb
+++ array_concate.rb
@@ -20,12 +20,11 @@ end
 puts "\nEnter the size of array 2\n"
 array_size = gets.chomp.to_i
 puts "\nEnter the Array Elements"
-second_input=Array.new(array_size)
+second_input = Array.new(array_size)
 
-(0...array_size).each do |input|
+array_size.times do |input|
   second_input[input]=gets.chomp
 end
 
 puts "\n"
 puts ArrayConcate.join_array(first_input, second_input)
-
