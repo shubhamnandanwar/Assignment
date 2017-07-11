64d11283ecd04159428e6dd58b5eb4c96ad072fa
ShubhamNandanwar <shubhamnandanwar9776@gmail.com>
2017-07-11 14:01:43 +0530

    Program to copy content of a csv file to a new csv file

--- copy_csv.rb
+++ copy_csv.rb
@@ -1,13 +1,14 @@
 require 'csv'
-
 class CopyFile
-	def self.create_duplicate_file(file_name)
-		CSV.open(file_name, "wb") do |output_row|
-			CSV.foreach('input.csv') do |row|
-				output_row << row
-			end
-		end
-	end
+  def self.create_duplicate_file(file_name)
+    
+    CSV.open(file_name, "wb") do |output_row|
+      output_row << CSV.open('input.csv', 'r') { |csv| csv.first }
+      CSV.foreach('input.csv', headers: true) do |row|
+      output_row << row
+      end
+    end
+  end
 end
 
 puts "Insert duplicate file name"
